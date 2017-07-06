f86731a3312e5bccb8ec3bcd6c9eda1cfcf94e28
Nabeel-PC\Nabeel <ns40002@gmail.com>
2017-07-06 21:19:29 +0200

    fixed the thumb spacing and fixed restricting being able to empty comment

--- js/comments.js
+++ js/comments.js
@@ -1,14 +1,18 @@
-$(document).ready(function(){
-$('#comments_submit_btn_js').click(function(){
-  var commentValue=$('#Text_area_input').val();
-  var hiddenDiv=$('.row.hidden');
-  console.log(commentValue);
-  var newComment=hiddenDiv.clone();
-  console.log(newComment);
-  hiddenDiv.before(newComment)
-  newComment.removeClass("hidden");
-  newComment.find('textarea').val(commentValue);
-$("#Text_area_input").val(" ");
-});
+$(document).ready(function() {
+  $('#comments_submit_btn_js').click(function() {
+      if ($.trim($("#Text_area_input").val())) {
+        console.log($.trim($("#Text_area_input").val()));
+        // textarea is empty or contains only white-space
+        var commentValue = $('#Text_area_input').val();
+        var hiddenDiv = $('.row.hidden.comments');
+        console.log(commentValue);
+        var newComment = hiddenDiv.clone();
+        console.log(newComment);
+        hiddenDiv.before(newComment)
+        newComment.removeClass("hidden");
+        newComment.find('textarea').val(commentValue);
+        $("#Text_area_input").val(" ");
+      }
+      });
 
 });
